14c91006c82229a61d7c41dc825fe81ea4f449ba
David <davidvo1990@gmail.com>
2019-04-16 02:10:49 -0500

    dv

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,4 +22,16 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
+    readProducts();
+    connection.end();
   });
+
+  function readProducts() {
+    console.log("Selecting all products...\n");
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      console.log(res);
+    //   connection.end();
+    });
+  }
